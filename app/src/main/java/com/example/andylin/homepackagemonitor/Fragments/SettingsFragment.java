f63f0db31354374078671b2490e89f5c5c0aa4e6
Leo <leobelanger1996@gmail.com>
2017-03-30 17:51:05 -0700

    Setting Fragment Complete - Needs Functionality with Database

--- app/src/main/java/com/example/andylin/homepackagemonitor/Fragments/SettingsFragment.java
+++ app/src/main/java/com/example/andylin/homepackagemonitor/Fragments/SettingsFragment.java
@@ -1,12 +1,16 @@
 package com.example.andylin.homepackagemonitor.Fragments;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.example.andylin.homepackagemonitor.R;
 import com.example.andylin.homepackagemonitor.View.Activities.MainActivity;
@@ -16,6 +20,12 @@ import com.example.andylin.homepackagemonitor.View.Activities.MainActivity;
  */
 
 public class SettingsFragment extends Fragment{
+    private Button accountSettingsButton;
+    private LinearLayout enableAccountLayout;
+    private LinearLayout accountLayout;
+    private boolean account_tab = false;
+    private Button account_dropdown_image;
+    private EditText phoneNumber;
 
     @Override
     public void onAttach(Context context) {
@@ -31,6 +41,32 @@ public class SettingsFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.content_settings_fragment, container, false);
+
+        enableAccountLayout = (LinearLayout) view.findViewById(R.id.enable_account_layout);
+        accountLayout = (LinearLayout) view.findViewById(R.id.account_layout);
+        accountSettingsButton = (Button) view.findViewById(R.id.account_button);
+
+        phoneNumber = (EditText) view.findViewById(R.id.phone_number_entry);
+        phoneNumber.setTypeface(Typeface.DEFAULT);
+
+        accountSettingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(account_tab == true) {
+                    accountLayout.setVisibility(View.GONE);
+                    account_tab = false;
+                    accountSettingsButton.setCompoundDrawablesWithIntrinsicBounds(R.mipmap.account_logo, 0, R.mipmap.dropdown_carrot, 0);
+                } else {
+                    accountLayout.setVisibility(View.VISIBLE);
+                    account_tab = true;
+                    accountSettingsButton.setCompoundDrawablesWithIntrinsicBounds(R.mipmap.account_logo, 0, R.mipmap.upward_arrow, 0);
+                }
+
+            }
+        });
+
+
+
         return view;
     }
 
